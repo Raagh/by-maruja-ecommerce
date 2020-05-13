e833e9876546eb2a40a267b008ba7451c9f699b4
Patricio Ferraggi <pattferraggi@gmail.com>
2020-05-13 23:46:13 +0200

    Feature/final frameworks configuration (#3)
    * basic structure web and sanity studio
    
    * basic call to sanity(not working)
    
    * remove sanity client log
    
    * fix tests
    
    * add sanity toke configuration
    
    * added styled components
    
    * added fp-ts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,136 +1,73 @@
 import axios from 'axios';
 import Head from 'next/head';
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import sanity from '../../lib/sanity';
 import { mock as paymentDataRequest } from '../../__mocks__/mercado-pago-create-payment-request.mock';
+
+const Container = styled.section`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Main = styled.main`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  text-align: center;
+`;
+
+const Description = styled.p`
+  line-height: 1.5;
+  font-size: 1.5rem;
+  text-align: center;
+`;
 
 const Index = (props: any) => {
   const [buttonText, setButtonText] = useState('');
 
   const contactCreatePayment = () => {
-    axios.post('http://localhost:3000/api/create-payment', paymentDataRequest).then((result) => setButtonText(result.data));
+    axios.post('/api/create-payment', paymentDataRequest).then((result) => setButtonText(result.data));
   };
 
   return (
-    <div id="home" className="container">
+    <Container>
       <Head>
-        <title>BY MARUJA TEST PROJECT</title>
+        <title>By Maruja</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
+      <Main>
+        <Title>
           Welcome to <a href="https://nextjs.org">Next.js! for by maruja ecommerce</a>
-        </h1>
-        <p className="description">
+        </Title>
+        <Description>
           Get started by editing <code>pages/index.tsx</code>
-        </p>
+        </Description>
         <button onClick={contactCreatePayment}>Call MercadoPago</button>
         <p id="buttonText">{buttonText}</p>
-        <p>
+        <Description>
           Nombre: {props.name} - Stock: {props.stock} - Precio: {props.price}
-        </p>
-      </main>
-
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Morte && Pato
-        </a>
-      </footer>
-
-      <style>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-      `}</style>
-
-      <style>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+        </Description>
+      </Main>
+    </Container>
   );
 };
 
-Index.getInitialProps = async function () {
+Index.getInitialProps = async () => {
   return await sanity.fetch(
     `
     *[_type == "product"][0]

962435ad72377c78034c5a189660e3e2f10d0133
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-05-15 14:35:39 -0700

    Styles applied

--- web/src/components/shared/navBar.tsx
+++ web/src/components/shared/navBar.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.section`
+  min-width: 100wh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const Bag = styled.img`
+  margin-right: 27px;
+  margin-top: 24px;
+`;
+
+const Logo = styled.img`
+  margin-top: 8px;
+`;
+
+const Menu = styled.img`
+  margin-left: 24px;
+  margin-top: 26px;
+`;
 
 const NavBar = () => {
     return (
-        <div>
-            <img src= "/assets/ICN-Menu 1.svg" alt = "Menu icon" />
-            <img src= "/assets/Logo.svg" alt = "Maruja Logo" />
-            <img src= "/assets/bag.svg" alt = "Bag icon" />    
-        </div>
+        <Container>
+            <Menu src= "/assets/ICN-Menu 1.svg" alt = "Menu icon" />
+            <Logo src= "/assets/Logo.svg" alt = "Maruja Logo" />
+            <Bag src= "/assets/bag.svg" alt = "Bag icon" />    
+        </Container>
     );
   };
 

2cb99dccce2f727c9dcb2d3a901181ef6dcddc3b
Patricio Ferraggi <pattferraggi@gmail.com>
2020-05-15 03:49:40 +0200

    Feature/final frameworks configuration (#4)
    * basic structure web and sanity studio
    
    * basic call to sanity(not working)
    
    * remove sanity client log
    
    * fix tests
    
    * add sanity toke configuration
    
    * added styled components
    
    * added fp-ts
    
    * add font and basic color config

---
+++ web/src/config/globalstyles.ts
@@ -0,0 +1,12 @@
+const darkBrown = '#3E3939';
+const darkPink = '#C79792';
+
+export const globalStyles = {
+  primaryFontName: 'Ortica',
+  primaryFontRegular: 400,
+  secondaryFontName: 'Mr Eaves XL Mod OT',
+  secondaryFontRegular: 400,
+  secondaryFontBold: 700,
+  primaryColor: darkBrown,
+  secondaryColor: darkPink,
+};
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,33 @@
 import axios from 'axios';
 import Head from 'next/head';
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import sanity from '../../lib/sanity';
 import { mock as paymentDataRequest } from '../../__mocks__/mercado-pago-create-payment-request.mock';
+import { globalStyles } from '../config/globalstyles';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100vh;
+  }
+
+  @font-face {
+    font-family: ${globalStyles.primaryFontName};
+    src: url('/fonts/Ortica-Light.woff') format("woff");
+  }
+
+  @font-face {
+    font-family: ${globalStyles.secondaryFontName};
+    src: url('/fonts/Mr-Eaves-XL-Modern.woff') format("woff");
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 const Container = styled.section`
   min-height: 100vh;
@@ -28,12 +52,27 @@ const Title = styled.h1`
   line-height: 1.15;
   font-size: 4rem;
   text-align: center;
+  font-family: ${globalStyles.primaryFontName};
+  font-weight: ${globalStyles.primaryFontRegular};
+  color: ${globalStyles.primaryColor};
 `;
 
-const Description = styled.p`
+const Subtitle = styled.p`
   line-height: 1.5;
   font-size: 1.5rem;
   text-align: center;
+  font-family: ${globalStyles.secondaryFontName};
+  font-weight: ${globalStyles.secondaryFontRegular};
+  color: ${globalStyles.secondaryColor};
+`;
+
+const SubtitleHighlight = styled.p`
+  line-height: 1.5;
+  font-size: 1.5rem;
+  text-align: center;
+  font-family: ${globalStyles.secondaryFontName};
+  font-weight: ${globalStyles.secondaryFontBold};
+  color: ${globalStyles.secondaryColor};
 `;
 
 const Index = (props: any) => {
@@ -45,23 +84,25 @@ const Index = (props: any) => {
 
   return (
     <Container>
+      <GlobalStyle></GlobalStyle>
       <Head>
         <title>By Maruja</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Main>
-        <Title>
-          Welcome to <a href="https://nextjs.org">Next.js! for by maruja ecommerce</a>
-        </Title>
-        <Description>
-          Get started by editing <code>pages/index.tsx</code>
-        </Description>
-        <button onClick={contactCreatePayment}>Call MercadoPago</button>
-        <p id="buttonText">{buttonText}</p>
-        <Description>
+        <Title>This is a title and uses the primary font in regular weight and with primary color (there is no primary font bold)</Title>
+        <Subtitle>This is a description and uses the secondary font in regular weight and secondary color</Subtitle>
+
+        <SubtitleHighlight>This is a subtitle highlight and uses the secondary font in bold weight and secondaryColor</SubtitleHighlight>
+        <br />
+        <p>
           Nombre: {props.name} - Stock: {props.stock} - Precio: {props.price}
-        </Description>
+        </p>
+        <br />
+        <button onClick={contactCreatePayment}>Call MercadoPago</button>
+        <br />
+        <p id="buttonText">{buttonText}</p>
       </Main>
     </Container>
   );

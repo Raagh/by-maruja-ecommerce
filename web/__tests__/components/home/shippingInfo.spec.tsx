2804c03163a36e4ea0ef26763eb03f7658af4100
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-05-21 15:36:35 -0700

    ShippingInfo renders 3 iconAndText and the h4

--- web/__tests__/components/home/shippingInfo.spec.tsx
+++ web/__tests__/components/home/shippingInfo.spec.tsx
@@ -8,4 +8,16 @@ describe('ShippingInfo component', () => {
 
     expect(wrapper.exists()).toBeTruthy();
   });
+
+  it('should render an h4', () => {
+    const wrapper = shallow(<ShippingInfo />);
+
+    expect(wrapper.render().find('h4').length).toEqual(1);
+  });
+
+  it('should render 3 iconAndText components', () => {
+    const wrapper = shallow(<ShippingInfo />);
+
+    expect(wrapper.find('IconAndText').length).toEqual(3);
+  });
 });
--- web/src/components/home/shippingInfo.tsx
+++ web/src/components/home/shippingInfo.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Container } from '../../config/global-styled-components';
+import { Container, StyledH4 } from '../../config/global-styled-components';
+import IconAndText from '../shared/iconAndText';
 
 const ShippingInfo = () => {
-  return <Container />;
+  const image = '/assets/Warehouse-delivery.svg';
+  const text1 = 'Comprá seguro con Mercado Pago, con tarjeta de crédito, débito o el que elijas';
+  const text2 = 'Mercado Envíos a toda la Argentina, gratis con compras de $500 o más';
+  const text3 = '¿Algún problema? Contactanos, nuestra atención es 100% personalizada';
+
+  return (
+    <Container>
+      <StyledH4>Bienvenida a Maruja</StyledH4>
+      <IconAndText image={image} text={text1} />
+      <IconAndText image={image} text={text2} />
+      <IconAndText image={image} text={text3} />
+    </Container>
+  );
 };
 
 export default ShippingInfo;

b2773aca1310ab7704b247da9234e8525c9d5776
Patricio Ferraggi <pattferraggi@gmail.com>
2020-05-22 17:09:37 +0200

    cleanup

--- web/__tests__/lamda-services/navbar.spec.tsx
+++ web/__tests__/lamda-services/navbar.spec.tsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import NavBar from '../../../src/components/home/navbar';
+import NavBar from '../../src/components/shared/navbar';
 
 describe('NavBar component', () => {
   it('should render', () => {
--- web/src/components/shared/layout.tsx
+++ web/src/components/shared/layout.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import React, { FunctionComponent } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { fonts } from '../../config/global-styles';
-import NavBar from '../home/navbar';
+import NavBar from './navbar';
 
 const GlobalStyle = createGlobalStyle`
   html,

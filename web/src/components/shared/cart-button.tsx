1903f336bbfb02af313575e3e8afa406ecc31265
Patricio Ferraggi <pattferraggi@gmail.com>
2020-06-01 13:57:25 +0200

    make styled components reusable

--- web/src/components/shared/cart-button.tsx
+++ web/src/components/shared/cart-button.tsx
@@ -7,14 +7,14 @@ const Container = styled.section`
   margin-top: 1.5rem;
   display: flex;
   flex-direction: row;
-  @media (${device.large}) {
+  @media ${device.large} {
     margin-left: auto;
     margin-right: 0.25rem;
   }
 `;
 
 const Bag = styled.img`
-  @media (${device.large}) {
+  @media ${device.large} {
     margin-top: 0.4rem;
     width: 14px;
     height: 17px;
@@ -26,7 +26,7 @@ const Links = styled(LinksSmall)`
   margin-top: 0.5rem;
   margin-right: 1.5rem;
   text-decoration: none;
-  @media (${device.large}) {
+  @media ${device.large} {
     display: block;
   }
 `;
--- web/src/config/global-styled-components.ts
+++ web/src/config/global-styled-components.ts
@@ -1,63 +1,84 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { typography, colors } from './global-styles';
+import { device } from './device';
 
-export const AlignedCenterContainer = styled.article`
-  text-align: center;
+const H1Styles = css`
+  font-family: ${typography.titles.h1.font.name};
+  font-style: normal;
+  font-weight: ${typography.titles.h1.font.regularWeight};
+  font-size: ${typography.titles.h1.fontSize};
+  line-height: ${typography.titles.h1.lineHeight};
+  color: ${colors.ui.darkSurface};
 `;
 
-// const StyledH1 = css`
-//   font-family: ${typography.titles.h1.font.name};
-//   font-style: normal;
-//   font-weight: ${typography.titles.h1.font.regularWeight};
-//   font-size: ${typography.titles.h1.fontSize};
-//   line-height: ${typography.titles.h1.lineHeight};
-//   color: ${colors.ui.darkSurface};
-// `;
+const H2Styles = css`
+  font-family: ${typography.titles.h2.font.name};
+  font-style: normal;
+  font-weight: ${typography.titles.h2.font.regularWeight};
+  font-size: ${typography.titles.h2.fontSize};
+  line-height: ${typography.titles.h2.lineHeight};
+  color: ${colors.ui.darkSurface};
+`;
 
-// const StyledH2 = css`
-//   font-family: ${typography.titles.h2.font.name};
-//   font-style: normal;
-//   font-weight: ${typography.titles.h2.font.regularWeight};
-//   font-size: ${typography.titles.h2.fontSize};
-//   line-height: ${typography.titles.h2.lineHeight};
-//   color: ${colors.ui.darkSurface};
-// `;
+const H3Styles = css`
+  font-family: ${typography.titles.h3.font.name};
+  font-style: normal;
+  font-weight: ${typography.titles.h3.font.regularWeight};
+  font-size: ${typography.titles.h3.fontSize};
+  line-height: ${typography.titles.h3.lineHeight};
+  color: ${colors.ui.darkSurface};
+`;
 
-// const StyledH3 = css`
-//   font-family: ${typography.titles.h3.font.name};
-//   font-style: normal;
-//   font-weight: ${typography.titles.h3.font.regularWeight};
-//   font-size: ${typography.titles.h3.fontSize};
-//   line-height: ${typography.titles.h3.lineHeight};
-//   color: ${colors.ui.darkSurface};
-// `;
+const H4Styles = css`
+  font-family: ${typography.titles.h4.font.name};
+  font-style: normal;
+  font-weight: ${typography.titles.h4.font.regularWeight};
+  font-size: ${typography.titles.h4.fontSize};
+  line-height: ${typography.titles.h4.lineHeight};
+  color: ${colors.ui.darkSurface};
+`;
 
-// const StyledH4 = css`
-//   font-family: ${typography.titles.h4.font.name};
-//   font-style: normal;
-//   font-weight: ${typography.titles.h4.font.regularWeight};
-//   font-size: ${typography.titles.h4.fontSize};
-//   line-height: ${typography.titles.h4.lineHeight};
-//   color: ${colors.ui.darkSurface};
-// `;
+const H5Styles = css`
+  font-family: ${typography.titles.h5.font.name};
+  font-style: normal;
+  font-weight: ${typography.titles.h5.font.regularWeight};
+  font-size: ${typography.titles.h5.fontSize};
+  line-height: ${typography.titles.h5.lineHeight};
+  color: ${colors.ui.darkSurface};
+`;
 
-// const StyledH5 = css`
-//   font-family: ${typography.titles.h5.font.name};
-//   font-style: normal;
-//   font-weight: ${typography.titles.h5.font.regularWeight};
-//   font-size: ${typography.titles.h5.fontSize};
-//   line-height: ${typography.titles.h5.lineHeight};
-//   color: ${colors.ui.darkSurface};
-// `;
-
-// const StyledH6 = css`
+// const H6Styles = css`
 //   font-family: ${typography.titles.h6.font.name};
 //   font-style: normal;
-//   font-weight: ${typography.titles.h6.font.regularWeight};
+//   font-weight: ${typography.titles.h6.font.fontWeight};
 //   font-size: ${typography.titles.h6.fontSize};
 //   line-height: ${typography.titles.h6.lineHeight};
 //   color: ${colors.ui.darkSurface};
 // `;
+
+export const StyledH1 = styled.h1`
+  ${H3Styles}
+
+  @media ${device.large} {
+    ${H1Styles}
+  }
+`;
+
+export const StyledH4 = styled.h4`
+  ${H5Styles}
+
+  @media ${device.large} {
+    ${H4Styles}
+  }
+`;
+
+export const StyledH2 = styled.h2`
+  ${H4Styles}
+
+  @media ${device.large} {
+    ${H2Styles}
+  }
+`;
 
 export const BodyCopyRegularSmall = styled.p`
   font-family: ${typography.bodyCopy.regular.font.name};
@@ -109,4 +130,8 @@ export const LinksSmall = styled.a`
   color: ${colors.ui.darkSurface};
 
   text-transform: ${typography.links.textTransform};
+`;
+
+export const AlignedCenterContainer = styled.article`
+  text-align: center;
 `;

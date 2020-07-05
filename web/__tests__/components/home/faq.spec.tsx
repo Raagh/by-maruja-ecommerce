e216ff14f4bb7bddbfd31e2743c3b7a82381bbcc
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-07-05 16:46:27 -0700

    Faq components renders a button and a title. Tested.

--- web/__tests__/components/home/faq.spec.tsx
+++ web/__tests__/components/home/faq.spec.tsx
@@ -1,11 +1,24 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 import Faq from '../../../src/components/home/faq';
+import Button from '../../../src/components/shared/button';
 
 describe('Faq component', () => {
   it('should render', () => {
     const wrapper = shallow(<Faq />);
 
     expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('should render 1 title', () => {
+    const wrapper = shallow(<Faq />);
+
+    expect(wrapper.render().find('h2').length).toEqual(1);
+  });
+
+  it('should render Button component', () => {
+    const wrapper = mount(<Faq />);
+
+    expect(wrapper.find(Button).length).toEqual(1);
   });
 });
--- web/src/components/home/faq.tsx
+++ web/src/components/home/faq.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import Button from '../shared/button';
+import { AlignedCenterContainer } from '../../config/global-styled-components';
+import { StyledH2 } from '../../config/global-styled-components';
+
+const FaqContainer = styled(AlignedCenterContainer)`
+  margin-top: 80px;
+`;
 
 const Faq = () => {
-  return <div />;
+  return (
+    <FaqContainer>
+      <StyledH2>Preguntas frecuentes</StyledH2>
+      <Button text={'LEER MÁS'} url={'/'} />
+    </FaqContainer>
+  );
 };
 
 export default Faq;

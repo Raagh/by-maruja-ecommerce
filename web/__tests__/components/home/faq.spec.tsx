9b4f796d7d5afe5dc0cfa29a8cb6a06d45db0336
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-07-06 16:48:18 -0700

    Faq renders as many Accordions as questions present in faq-conf.json file.

--- web/__tests__/components/home/faq.spec.tsx
+++ web/__tests__/components/home/faq.spec.tsx
@@ -2,6 +2,8 @@ import { shallow, mount } from 'enzyme';
 import React from 'react';
 import Faq from '../../../src/components/home/faq';
 import Button from '../../../src/components/shared/button';
+import Accordion from '../../../src/components/shared/accordion';
+import FaqJson from '../../../src/config/faq-conf.json';
 
 describe('Faq component', () => {
   it('should render', () => {
@@ -14,6 +16,12 @@ describe('Faq component', () => {
     const wrapper = shallow(<Faq />);
 
     expect(wrapper.render().find('h2').length).toEqual(1);
+  });
+
+  it('should render an equal number of Accordions as questions are in faq-conf.json file', () => {
+    const wrapper = mount(<Faq />);
+
+    expect(wrapper.find(Accordion).length).toEqual(FaqJson.data.length);
   });
 
   it('should render Button component', () => {
--- web/src/components/home/faq.tsx
+++ web/src/components/home/faq.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from '../shared/button';
+import Accordion from '../shared/accordion';
 import { AlignedCenterContainer } from '../../config/global-styled-components';
 import { StyledH2 } from '../../config/global-styled-components';
+import FaqJson from '../../config/faq-conf.json';
 
 const FaqContainer = styled(AlignedCenterContainer)`
   margin-top: 80px;
 `;
 
+const createAccordionContent = (question: string, answer: string, index: number) => {
+  console.log(question, answer);
+  return <Accordion key={index} />;
+};
+
 const Faq = () => {
   return (
     <FaqContainer>
       <StyledH2>Preguntas frecuentes</StyledH2>
+      {FaqJson.data.map((question, index) => createAccordionContent(question.question, question.answer, index))}
       <Button text={'LEER MÁS'} url={'/'} />
     </FaqContainer>
   );

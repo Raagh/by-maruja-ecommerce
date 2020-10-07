b4078da65d07f7bc539c8bef46c5b363d0c4a492
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-10-07 14:35:43 -0700

    Bugfixes

--- web/src/lamda-services/send-email.ts
+++ web/src/lamda-services/send-email.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-export const sendEmail = async ({ name, email, content }: { name: string; email: string; content: string }) => {
+export const sendEmail = ({ name, email, content }: { name: string; email: string; content: string }) => {
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -16,11 +16,13 @@ export const sendEmail = async ({ name, email, content }: { name: string; email:
     text: `El usuario ${name} envio el siguiente mensaje: ${content}. Responder a ${email}`,
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      return error;
-    } else {
-      return 'Email sent: ' + info.response;
-    }
-  });
+  const sendMailPromise = () => {
+    return new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, (error, info) => {
+        return error ? reject(error) : resolve(info);
+      });
+    });
+  };
+
+  return sendMailPromise();
 };
--- web/src/pages/api/send-email.ts
+++ web/src/pages/api/send-email.ts
@@ -6,14 +6,13 @@ import { ErrorMessages } from '../../config/error-messages';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const { name, email, content } = req.body;
-    await sendEmail({ name, email, content });
-    return res.status(200).end();
-  } else {
-    return res.status(404).json({
-      error: {
-        code: 'not_found',
-        message: ErrorMessages.EndpointMethodIncorrect,
-      },
-    });
+    const result = await sendEmail({ name, email, content });
+    return res.status(200).send(result);
   }
+  return res.status(404).json({
+    error: {
+      code: 'not_found',
+      message: ErrorMessages.EndpointMethodIncorrect,
+    },
+  });
 };

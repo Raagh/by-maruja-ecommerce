f6f8c297793b0afbf58e6236ffc982bcebc0e60e
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-10-19 10:43:16 -0700

    Desktop Styles completed

--- web/src/pages/contact.tsx
+++ web/src/pages/contact.tsx
@@ -9,10 +9,20 @@ import Faq from '../components/shared/faq';
 import { CategoryConfiguration } from '../model/category-configuration';
 import { StyledH3Title, BodyCopyBoldSmall, BodyCopyRegularSmall, StyledH6Title } from '../config/global-styled-components';
 import { colors } from '../config/global-styles';
+import { device } from '../config/device';
 
 
 const ContactPageContainer = styled.section`
     padding: 0 1.5rem 0 1.5rem;
+`;
+
+const LargeOnlyContainer = styled.div`
+    @media ${device.large} {
+        display: flex;
+        flex-flow: row-reverse;
+        justify-content: center;
+        margin-bottom: 5rem;
+    }
 `;
 
 const ContactPageTitle = styled(StyledH3Title)`
@@ -22,16 +32,29 @@ const ContactPageTitle = styled(StyledH3Title)`
   margin-bottom: 2rem;
 `;
 
-const ContactForm = styled.form``;
+const ContactForm = styled.form`
+    @media ${device.large} {
+        max-width: 400px;
+        width: 100%;
+    }
+`;
 
-const ContactHeader = styled(BodyCopyRegularSmall)`
+const ContactHeader = styled(BodyCopyRegularSmall)<{ onlyMobile: boolean}>`
     font-size: 16px;
     text-align: center;
     margin-bottom: 2.5rem;
+    display: ${(props) => props.onlyMobile ? 'inline-block' : 'none'};
+    @media ${device.large} {
+        display: ${(props) => props.onlyMobile ? 'none' : 'inline-block'};
+    }
 `;
 
 const SocialNetworksContainer = styled.section`
     margin-top: 3.5rem;
+    @media ${device.large} {
+        max-width: 350px;
+        margin-right: 5rem;
+    }
 `;
 
 const SocialNetwork = styled.div`
@@ -61,14 +84,16 @@ const ContactPage = ({ categories }: { categories: Array<CategoryConfiguration> 
     <Layout categories={categories}>
       <ContactPageTitle>Contacto</ContactPageTitle>
       <ContactPageContainer>
+          <LargeOnlyContainer>
           <ContactForm autoComplete="off">
-                <ContactHeader>Llená el formulario para contactarnos. Te vamos a responder dentro de las próximas 24 horas hábiles.</ContactHeader>
+                <ContactHeader onlyMobile={true}>Llená el formulario para contactarnos. Te vamos a responder dentro de las próximas 24 horas hábiles.</ContactHeader>
                 <FormInput name={'Nombre'} type={'text'} />
                 <FormInput name={'E-mail'} type={'email'} />
                 <FormInput name={'Mensaje'} type={'text'} big={true}/>
                 <PrimaryButton text='ENVIAR MENSAJE' onClick={() => console.log('Here')}/>
           </ContactForm>
           <SocialNetworksContainer>
+            <ContactHeader onlyMobile={false}>Llená el formulario para contactarnos. Te vamos a responder dentro de las próximas 24 horas hábiles.</ContactHeader>
             <SocialNetwork>
                 <SocialNetworkTitle>CONTACTANOS POR INSTAGRAM</SocialNetworkTitle>
                 <SocialNetworkHeader><Icon src='/assets/Instagram.svg'/>@ByMaruja</SocialNetworkHeader>
@@ -78,6 +103,7 @@ const ContactPage = ({ categories }: { categories: Array<CategoryConfiguration> 
                 <SocialNetworkHeader><Icon src='/assets/Instagram.svg'/>hola@maruja.com</SocialNetworkHeader>
             </SocialNetwork>
           </SocialNetworksContainer>
+          </LargeOnlyContainer>
         <Faq/>
       </ContactPageContainer>
     </Layout>
